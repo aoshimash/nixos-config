aefb801b2a1742f803171a5ef56b7020ff738b2f
Shuji Aoshima <47586723+aoshimash@users.noreply.github.com>
2026-03-28 00:08:22 +0900

    feat: migrate rofi to catppuccin theming, remove dracula.rasi

--- home/rofi.nix
+++ home/rofi.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }:
 {
+  catppuccin.rofi.enable = true;
+
   programs.rofi = {
     enable = true;
     package = pkgs.rofi;
     terminal = "ghostty";
-    theme = ./dotfiles/rofi/dracula.rasi;
   };
 }
